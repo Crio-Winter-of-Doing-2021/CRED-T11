c18e1e9a3c95f23aa4f03693358985e8391bee7d
ranjit <ranjitmaheshshah@gmail.com>
2021-03-10 01:37:49 +0530

    replace id with UUID

--- cred-backend/app/models/user.model.js
+++ cred-backend/app/models/user.model.js
@@ -1,5 +1,13 @@
+const uuid = require("uuidv4");
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
     username: {
       type: Sequelize.STRING,
     },
@@ -10,6 +18,8 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
   });
+
+  //   User.beforeCreate((user) => (user.id = uuid()));
 
   return User;
 };
--- cred-backend/server.js
+++ cred-backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -19,12 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // database
 const db = require("./app/models");
 
-
 // db.sequelize.sync();
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
-});
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+// });
 
 // simple route
 app.get("/", (req, res) => {
@@ -32,11 +31,10 @@ app.get("/", (req, res) => {
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
+require("./app/routes/auth.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
