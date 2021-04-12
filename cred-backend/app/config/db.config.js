ea48adf36812fd7d580167036176ab736845c547
Devansh Chaubey <devkumar5436@gmail.com>
2021-04-12 14:49:10 +0530

    final version push

--- cred-backend/app/config/db.config.js
+++ cred-backend/app/config/db.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234",
-  DB: "cred",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DATABASE,
   dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
